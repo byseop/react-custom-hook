35e5d00af9c4a12199ebbe9c46bdaa8cedc31318
byseop <byseop@gmail.com>
2019-12-17 10:42:49 +0900

    Initial upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UseInputsExample from './hooks/form/UseInputsExample';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <UseInputsExample />
+    </div>
+  );
+}
+
+export default App;
